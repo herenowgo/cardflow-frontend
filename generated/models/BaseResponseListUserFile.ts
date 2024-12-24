f32dcfc0d5e69cb4d285e1f5fa340d583bd88fd8
qiuqiu <your@email.com>
2024-12-24 16:48:16 +0800

    feat(generated/models): 添加文件预览和用户文件模型

---
+++ generated/models/BaseResponseListUserFile.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { UserFile } from './UserFile';
+
+export type BaseResponseListUserFile = {
+    code?: number;
+    data?: Array<UserFile>;
+    message?: string;
+};
+
---
+++ generated/models/UserFile.ts
@@ -0,0 +1,21 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type UserFile = {
+    id?: string;
+    userId?: number;
+    name?: string;
+    path?: string;
+    type?: string;
+    size?: number;
+    coverPath?: string;
+    parentPath?: string;
+    isFolder?: boolean;
+    isDeleted?: boolean;
+    createTime?: string;
+    updateTime?: string;
+    deleteTime?: string;
+};
+
