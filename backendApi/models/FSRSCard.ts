4e680f4ec6ad4b9cfb34e8f6047fd08e49ba212e
qiuqiu <your@email.com>
2025-02-23 20:11:30 +0800

    对FSRS相关实体添加注释

--- backendApi/models/FSRSCard.ts
+++ backendApi/models/FSRSCard.ts
@@ -3,15 +3,45 @@
 /* tslint:disable */
 /* eslint-disable */
 
+/**
+ * FSRS卡片实体
+ */
 export type FSRSCard = {
+    /**
+     * 卡片下次复习的日期
+     */
     due?: string;
+    /**
+     * 记忆稳定性
+     */
     stability?: number;
+    /**
+     * 卡片难度
+     */
     difficulty?: number;
+    /**
+     * 自上次复习以来的天数
+     */
     elapsed_days?: number;
+    /**
+     * 下次复习的间隔天数
+     */
     scheduled_days?: number;
+    /**
+     * 卡片被复习的总次数
+     */
     reps?: number;
+    /**
+     * 卡片被遗忘或错误记忆的次数
+     */
     lapses?: number;
+    /**
+     * 卡片的当前状态（新卡片、学习中、复习中、重新学习中）
+     */
     state?: string;
+    /**
+     * 最近一次复习的日期
+     */
     last_review?: string;
 };
 
--- backendApi/models/ReviewLogDTO.ts
+++ backendApi/models/ReviewLogDTO.ts
@@ -4,15 +4,45 @@
 /* eslint-disable */
 
 export type ReviewLogDTO = {
+    /**
+     * 关联的卡片ID
+     */
     cardId?: string;
+    /**
+     * 复习的评级（手动变更，重来，困难，良好，容易）
+     */
     rating?: string;
+    /**
+     * 复习的状态（新卡片、学习中、复习中、重新学习中）
+     */
     state?: string;
+    /**
+     * 上次的调度日期
+     */
     due?: string;
+    /**
+     * 复习前的记忆稳定性
+     */
     stability?: number;
+    /**
+     * 复习前的卡片难度
+     */
     difficulty?: number;
+    /**
+     * 自上次复习以来的天数
+     */
     elapsed_days?: number;
+    /**
+     * 上次复习的间隔天数
+     */
     last_elapsed_days?: number;
+    /**
+     * 下次复习的间隔天数
+     */
     scheduled_days?: number;
+    /**
+     * 复习的日期
+     */
     review?: string;
 };
 
